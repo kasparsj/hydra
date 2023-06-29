f0178134ceb3f686bf06be7f5273d61a13f666a9
Kaspars <kasparsj@gmail.com>
2023-06-29 15:46:44 +0300

    add Mac keys, switch to dev

--- src/views/editor/keymaps.js
+++ src/views/editor/keymaps.js
@@ -7,5 +7,14 @@ module.exports = {
     'Shift-Ctrl-F': 'editor:formatCode',
     'Shift-Ctrl-L': 'gallery:saveToURL',
     'Shift-Ctrl-H': 'hideAll',
-    'Shift-Ctrl-S': 'screencap'
+    'Shift-Ctrl-S': 'screencap',
+
+    'Cmd-Enter': 'editor:evalLine',
+    'Cmd-/': 'editor:toggleComment',
+    'Shift-Cmd-Enter': 'editor:evalAll',
+    'Shift-Cmd-G': 'gallery:shareSketch',
+    'Shift-Cmd-F': 'editor:formatCode',
+    'Shift-Cmd-L': 'gallery:saveToURL',
+    'Shift-Cmd-H': 'hideAll',
+    'Shift-Cmd-S': 'screencap'
 }
